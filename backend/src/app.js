d1175e097074534d1e662a86214652170859b6f1
Abhay Singh <abhaysingh14922@gmail.com>
2026-07-29 16:46:53 +0530

    update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,15 +4,20 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+
+// Ignore browser requests for favicons to prevent unnecessary function crashes
+app.get('/favicon.png', (req, res) => res.status(204).end());
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+
 app.use(cors({
-    origin: process.env.NODE_ENV === 'production' ? 'https://noteapp-m521.vercel.app' : 'http://localhost:5173',
+    origin: process.env.NODE_ENV === 'production' ? 'https://noteapp-dun-chi.vercel.app' : 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.options('*', cors({
-    origin: process.env.NODE_ENV === 'production' ? 'https://noteapp-m521.vercel.app' : 'http://localhost:5173',
+    origin: process.env.NODE_ENV === 'production' ? 'https://noteapp-dun-chi.vercel.app' : 'http://localhost:5173',
     credentials: true
 }));
 
